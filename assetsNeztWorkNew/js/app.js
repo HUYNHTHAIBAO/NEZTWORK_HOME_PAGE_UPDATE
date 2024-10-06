25ee9556dc4cf53d257f1bfc2caed2f1f1de5c4c
Huỳnh Thái Bảo <huynhthaibao.it@gmail.com>
2024-10-06 23:44:34 +0700

    up

--- assetsNeztWorkNew/js/app.js
+++ assetsNeztWorkNew/js/app.js
@@ -410,3 +410,43 @@
       
         // Bắt đầu chạy chữ khi trang tải
         window.onload = typePlaceholder;
+
+
+        $(function () {
+          $.datepicker.regional["vi-VN"] = {
+              closeText: "Đóng",
+              prevText: "Trước",
+              nextText: "Sau",
+              currentText: "Hôm nay",
+              monthNames: ["Tháng một", "Tháng hai", "Tháng ba", "Tháng tư", "Tháng năm", "Tháng sáu", "Tháng bảy", "Tháng tám", "Tháng chín", "Tháng mười", "Tháng mười một", "Tháng mười hai"],
+              monthNamesShort: ["Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6", "Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"],
+              dayNames: ["Chủ nhật", "Thứ hai", "Thứ ba", "Thứ tư", "Thứ năm", "Thứ sáu", "Thứ bảy"],
+              dayNamesShort: ["CN", "Hai", "Ba", "Tư", "Năm", "Sáu", "Bảy"],
+              dayNamesMin: ["CN", "T2", "T3", "T4", "T5", "T6", "T7"],
+              weekHeader: "Tuần",
+              dateFormat: "dd/mm/yy",
+              firstDay: 1,
+              isRTL: false,
+              showMonthAfterYear: false,
+              yearSuffix: ""
+          };
+  
+          $.datepicker.setDefaults($.datepicker.regional["vi-VN"]);
+          // Lấy năm hiện tại
+          var currentYear = new Date().getFullYear();
+          // Đặt ngày bắt đầu của năm hiện tại
+          var minDate = new Date(currentYear, 0, 1);
+          $(".datepicker-popup").datepicker({
+              dateFormat: "dd-mm-yy",
+              duration: "fast",
+              changeMonth: true, // Cho phép chọn tháng
+              changeYear: true,  // Cho phép chọn năm
+              showOtherMonths: true,
+              selectOtherMonths: true,
+              showAnim: "fadeIn",
+              minDate: new Date(), // Cấm chọn ngày trước ngày hiện tại
+  
+          }).datepicker("show");
+  
+  
+      });
